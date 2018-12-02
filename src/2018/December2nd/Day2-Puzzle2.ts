a79b921901a21993571aceae06c0d2345dbf1565
mfbieber <mfbieber@gmail.com>
2018-12-02 15:43:28 +0100

    Solves Puzzle 2 of Day 2

---
+++ src/2018/December2nd/Day2-Puzzle2.ts
@@ -0,0 +1,32 @@
+{const fs = require('fs');
+
+let array : String[] = fs.readFileSync('src/2018/December2nd/input').toString().split("\n");
+let sameCharsArray : String[] = [];
+
+let lineLength : number = array[0].length;
+
+
+for (let i = 0; i < array.length; i++) {
+    let lineToCheck : String = array[i];
+    for (let j = i + 1; j < array.length; j++) {
+        let sameChars: String = '';
+        let differentChars: String = '';
+        let lineToCompareTo : String = array[j];
+        for (let k = 0; k < lineLength; k++) {
+            if (lineToCheck.charAt(k) == lineToCompareTo.charAt(k)) {
+                sameChars = sameChars.concat(lineToCheck.charAt(k));
+            } else {
+                differentChars = differentChars.concat(lineToCheck.charAt(k));
+            }
+        }
+        console.log('lineToCheck: ' + lineToCheck + ' - lineToCompareTo: ' + lineToCompareTo + ' - sameChars: ' + sameChars + ' - differentChars: ' + differentChars);
+        sameCharsArray.push(sameChars);
+    }
+}
+
+for (let results of sameCharsArray) {
+    if (results.length == lineLength - 1) {
+        console.log(results);
+    }
+}
+}
