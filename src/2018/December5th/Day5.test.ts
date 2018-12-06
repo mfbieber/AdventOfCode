ad1a856848567ded39972ee2214483cb94e52b3d
mfbieber <mfbieber@gmail.com>
2018-12-06 18:07:57 +0100

    Solves Puzzle 2 of Day 5

--- src/2018/December5th/Day5.test.ts
+++ src/2018/December5th/Day5.test.ts
@@ -1,4 +1,4 @@
-import {performAlchemicalReaction} from "./Day5";
+import {performAlchemicalReaction, performTrimming, bestTrimmedPolymer} from "./Day5";
 import {expect} from "chai";
 
 describe('Alchemical reactions', () => {
@@ -7,7 +7,7 @@ describe('Alchemical reactions', () => {
     const testPolymer2: string = 'dabAcCaaaaaaaaCBAcCcaDA';
 
 
-        it('should perform the alchemical reactions', () => {
+    it('should perform the alchemical reactions', () => {
         let reactedPolymer: string = performAlchemicalReaction(testPolymer);
         let reactedPolymer2: string = performAlchemicalReaction(testPolymer2);
 
@@ -16,6 +16,24 @@ describe('Alchemical reactions', () => {
         console.log('resulting polymer: ' + reactedPolymer + ', polymer length: ' + reactedPolymer.length);
         expect(reactedPolymer2).to.deep.equal('dabaaaaaaaCBAcaDA');
         console.log('resulting polymer: ' + reactedPolymer2 + ', polymer length: ' + reactedPolymer2.length);
+    });
+
+    it('trims the polymers', () => {
+        let expectedTrimmedPolymers : {[key: string] : string} = {};
+        expectedTrimmedPolymers['a'] = 'dbcCCBcCcD';
+        expectedTrimmedPolymers['b'] = 'daAcCaCAcCcaDA';
+        expectedTrimmedPolymers['c'] = 'dabAaBAaDA';
+        expectedTrimmedPolymers['d'] = 'abAcCaCBAcCcaA';
+
+        let trimmedPolymers : {[key: string] : string} = performTrimming(testPolymer);
+
+        expect(trimmedPolymers).to.deep.equal(expectedTrimmedPolymers);
+    });
+
+    it('finds the best trimmed polymer', () => {
+        let shortestReactionProductLength : number = bestTrimmedPolymer(testPolymer);
+
+        expect(shortestReactionProductLength).to.equal(4);
     });
 }
 )
--- src/2018/December5th/Day5.ts
+++ src/2018/December5th/Day5.ts
@@ -1,3 +1,35 @@
+export function bestTrimmedPolymer(startingMaterial : string) : number {
+    let trimmedStaringMaterials : {[key: string] : string} = performTrimming(startingMaterial);
+    let shortestReactionProductLength : number = startingMaterial.length;
+    for(let trimmedStartingMaterial in trimmedStaringMaterials) {
+        let reactionProductLength: number = performAlchemicalReaction(trimmedStaringMaterials[trimmedStartingMaterial]).length;
+        if(reactionProductLength < shortestReactionProductLength) {
+            shortestReactionProductLength = reactionProductLength;
+        }
+    }
+    return shortestReactionProductLength;
+}
+
+export function performTrimming(startingMaterial: string) : {[key: string] : string} {
+    let alphabet : string[] = ['a' ,'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let trimmedPolymers : {[key: string] : string} = {};
+    for(let letter of alphabet) {
+        let trimmedPolymer : string = '';
+        let trimmingPerformed : boolean = false;
+        for (let position = 0; position < startingMaterial.length; position++) {
+            if (startingMaterial.charAt(position).toLowerCase() !== letter) {
+                trimmedPolymer = trimmedPolymer + startingMaterial.slice(position, position + 1)
+            }  else {
+                trimmingPerformed = true;
+            }
+        }
+        if(trimmingPerformed) {
+            trimmedPolymers[letter] = trimmedPolymer;
+        }
+    }
+    return trimmedPolymers;
+}
+
 export function performAlchemicalReaction(startingMaterial : string) : string {
     let reactionStep : Reaction = performAlchemicalReactionStep(startingMaterial);
     let reactionPerformed : boolean = reactionStep.reactionPerformed;
