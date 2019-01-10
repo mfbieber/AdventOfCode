cdd6b2996558391b9a684f47aa3bd4b2ddc11a7a
mfbieber <mfbieber@gmail.com>
2019-01-10 15:08:59 +0100

    WIP: Puzzle 1 of Day 13

---
+++ src/2018/December13th/Day13.test.ts
@@ -0,0 +1,218 @@
+import {Track, Cart} from "./Day13";
+import {expect} from "chai";
+
+describe('Mine Cart Madness', () => {
+    let straightTrackInput : string =
+        '|\n' +
+        'v\n' +
+        '|\n' +
+        '|\n' +
+        '|\n' +
+        '^\n' +
+        '|';
+
+    let complexTrackInput : string =
+        '/->-\\        \n' +
+        '|   |  /----\\\n' +
+        '| /-+--+-\\  |\n' +
+        '| | |  | v  |\n' +
+        '\\-+-/  \\-+--/\n' +
+        '  \\------/   ';
+
+    it ('finds the position of the first crash', () => {
+        let complexTrack : Track = new Track(complexTrackInput);
+
+        complexTrack.runRace();
+
+        expect(complexTrack.firstCrash).to.deep.equal(7 + ', ' + 3);
+    });
+
+    it ('can make carts move on the more complex test track', () => {
+        let complexTrack : Track = new Track(complexTrackInput);
+        complexTrack.moveCarts();
+
+        let outcomeAfterOneMove : Track = new Track(
+            '/-->\\        \n' +
+            '|   |  /----\\\n' +
+            '| /-+--+-\\  |\n' +
+            '| | |  | |  |\n' +
+            '\\-+-/  \\->--/\n' +
+            '  \\------/   ');
+
+        expect(complexTrack.coordinates).to.deep.equal(outcomeAfterOneMove.coordinates);
+
+        complexTrack.moveCarts();
+
+        let ouctomeAfterTwoMoves : Track = new Track(
+            '/---v        \n' +
+            '|   |  /----\\\n' +
+            '| /-+--+-\\  |\n' +
+            '| | |  | |  |\n' +
+            '\\-+-/  \\-+>-/\n' +
+            '  \\------/   ');
+
+        expect(complexTrack.coordinates).to.deep.equal(ouctomeAfterTwoMoves.coordinates);
+
+        complexTrack.moveCarts();
+
+        let outcomeAfterThreeMoves : Track = new Track(
+            '/---\\        \n' +
+            '|   v  /----\\\n' +
+            '| /-+--+-\\  |\n' +
+            '| | |  | |  |\n' +
+            '\\-+-/  \\-+->/\n' +
+            '  \\------/   ');
+
+        expect(complexTrack.coordinates).to.deep.equal(outcomeAfterThreeMoves.coordinates);
+
+        complexTrack.moveCarts();
+
+        let outcomeAfterFourMoves : Track = new Track(
+            '/---\\        \n' +
+            '|   |  /----\\\n' +
+            '| /->--+-\\  |\n' +
+            '| | |  | |  |\n' +
+            '\\-+-/  \\-+--^\n' +
+            '  \\------/   ');
+
+        expect(complexTrack.coordinates).to.deep.equal(outcomeAfterFourMoves.coordinates);
+
+        complexTrack.moveCarts();
+
+        let outcomeAfterFiveMoves : Track = new Track(
+            '/---\\        \n' +
+            '|   |  /----\\\n' +
+            '| /-+>-+-\\  |\n' +
+            '| | |  | |  ^\n' +
+            '\\-+-/  \\-+--/\n' +
+            '  \\------/   ');
+
+        expect(complexTrack.coordinates).to.deep.equal(outcomeAfterFiveMoves.coordinates);
+
+        complexTrack.moveCarts();
+
+        let outcomeAfterSixMoves : Track = new Track(
+            '/---\\        \n' +
+            '|   |  /----\\\n' +
+            '| /-+->+-\\  ^\n' +
+            '| | |  | |  |\n' +
+            '\\-+-/  \\-+--/\n' +
+            '  \\------/   ');
+
+        expect(complexTrack.coordinates).to.deep.equal(outcomeAfterSixMoves.coordinates);
+
+        complexTrack.moveCarts();
+
+        let outcomeAfterSevenMoves : Track = new Track(
+            '/---\\        \n' +
+            '|   |  /----<\n' +
+            '| /-+-->-\\  |\n' +
+            '| | |  | |  |\n' +
+            '\\-+-/  \\-+--/\n' +
+            '  \\------/   ');
+
+        expect(complexTrack.coordinates).to.deep.equal(outcomeAfterSevenMoves.coordinates);
+
+        for (let i = 7; i < 14; i++) {
+            complexTrack.moveCarts();
+        }
+
+        let outcomeAfterFourteenMoves : Track = new Track(
+            '/---\\        \n' +
+            '|   |  /----\\\n' +
+            '| /-+--+-\\  |\n' +
+            '| | |  X |  |\n' +
+            '\\-+-/  \\-+--/\n' +
+            '  \\------/   ');
+
+        expect(complexTrack.coordinates).to.deep.equal(outcomeAfterFourteenMoves.coordinates);
+    });
+
+    it ('can make carts move on the straight line test track', () => {
+        let straightTrack : Track = new Track(straightTrackInput);
+        straightTrack.moveCarts();
+
+        let firstCartSet : Set<Cart> = new Set();
+        firstCartSet.add(new Cart('v', 0, 2));
+        firstCartSet.add(new Cart('^', 0, 4));
+
+        expect(straightTrack.carts).to.deep.equal(firstCartSet);
+
+        straightTrack.moveCarts();
+
+        let secondCartSet : Set<Cart> = new Set();
+        secondCartSet.add(new Cart('v', 0, 3));
+        secondCartSet.add(new Cart('^', 0, 3));
+
+        expect(straightTrack.carts).to.deep.equal(secondCartSet);
+        expect(straightTrack.coordinates[3][0]).to.equal('X');
+    });
+
+    it ('can make carts and position them correctly from the straight line test input', () => {
+        let straightTrack : Track = new Track(straightTrackInput);
+
+        let topCart : Cart = new Cart('v', 0, 1);
+        let bottomCart : Cart = new Cart('^', 0, 5);
+        let cartSet : Set<Cart> = new Set();
+        cartSet.add(topCart);
+        cartSet.add(bottomCart);
+
+       expect(straightTrack.carts).to.deep.equal(cartSet);
+    });
+
+    it ('can make carts and position them correctly from the more complex test input', () => {
+        let complexTrack : Track = new Track(complexTrackInput);
+
+        let cartSet : Set<Cart> = new Set();
+        cartSet.add(new Cart('>', 2, 0));
+        cartSet.add(new Cart('v', 9, 3));
+
+        expect(complexTrack.carts).to.deep.equal(cartSet);
+    });
+
+    it('can build up a track', () => {
+        let expectedTrackOutput : Track = new Track('');
+        expectedTrackOutput.coordinates = [
+            ['/', '-', '-', '-', '-', '\\'],
+            ['|', ' ', ' ', ' ', ' ', '|'],
+            ['|', ' ', ' ', ' ', ' ', '|'],
+            ['\\', '-', '-', '-', '-', '/'],
+        ];
+
+        let trackInput : string =
+            '/----\\\n' +
+            '|    |\n' +
+            '|    |\n' +
+            '\\----/';
+
+        let track : Track = new Track(trackInput);
+
+        expect(track.coordinates).to.deep.equal(expectedTrackOutput.coordinates);
+    });
+
+    it('can build up a more complex track', () => {
+        let expectedTrackOutput : Track = new Track('');
+        expectedTrackOutput.coordinates = [
+            ['/', '-', '-', '-', '-', '-', '\\', ' ', ' ', ' '],
+            ['|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' '],
+            ['|', ' ', ' ', '/', '-', '-', '+', '-', '-', '\\'],
+            ['|', ' ', ' ', '|', ' ', ' ', '|', ' ', ' ', '|'],
+            ['\\', '-', '-', '+', '-', '-', '/', ' ', ' ', '|'],
+            [' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'],
+            [' ', ' ', ' ', '\\', '-', '-', '-', '-', '-', '/']
+        ];
+
+        let trackInput : string =
+            '/-----\\\n' +
+            '|     |\n' +
+            '|  /--+--\\\n' +
+            '|  |  |  |\n' +
+            '\\--+--/  |\n' +
+            '   |     |\n' +
+            '   \\-----/';
+
+        let track : Track = new Track(trackInput);
+
+        expect(track.coordinates).to.deep.equal(expectedTrackOutput.coordinates);
+    });
+});
---
+++ src/2018/December13th/Day13.ts
@@ -0,0 +1,169 @@
+export class Track{
+    coordinates : string[][] = [];
+    carts : Set<Cart> = new Set();
+    cleanTrackCoordinates : string[][] = [];
+    firstCrash : string = '';
+
+    public runRace() {
+        while (this.firstCrash == '') {
+            this.moveCarts();
+        }
+    }
+
+    public moveCarts() {
+        let orderedCarts : Cart[] = [];
+        for (let y = 0; y < this.coordinates.length; y++) {
+            for (let x = 0; x < this.coordinates[y].length; x++) {
+                for (let cart of this.carts) {
+                    if (cart.xPosition == x && cart.yPosition == y) {
+                        orderedCarts.push(cart);
+                    }
+                }
+            }
+        }
+        for (let cart of orderedCarts) {
+            this.moveToNextPosition(cart);
+            this.changeDirection(cart);
+        }
+    }
+
+    public moveToNextPosition(cart : Cart) {
+        this.coordinates[cart.yPosition][cart.xPosition] = this.cleanTrackCoordinates[cart.yPosition][cart.xPosition];
+        if (cart.direction == '^') {
+            cart.yPosition = cart.yPosition - 1;
+        } else if (cart.direction == '>') {
+            cart.xPosition = cart.xPosition + 1;
+        } else if (cart.direction == 'v') {
+            cart.yPosition = cart.yPosition + 1;
+        } else if (cart.direction == '<') {
+            cart.xPosition = cart.xPosition - 1;
+        }
+    }
+
+    public changeDirection(cart: Cart) {
+        let trackCoordinate : string = this.coordinates[cart.yPosition][cart.xPosition];
+        if (trackCoordinate == '/' && cart.direction == '^') {
+            cart.direction = '>';
+        } else if (trackCoordinate == '/' && cart.direction == '<') {
+            cart.direction = 'v';
+        } else if (trackCoordinate == '/' && cart.direction == 'v') {
+            cart.direction = '<';
+        } else if (trackCoordinate == '/' && cart.direction == '>') {
+            cart.direction = '^';
+        }
+
+        if (trackCoordinate == '\\' && cart.direction == '<') {
+            cart.direction = '^';
+        } else if (trackCoordinate == '\\' && cart.direction == '^') {
+            cart.direction = '<';
+        } else if (trackCoordinate == '\\' && cart.direction == '>') {
+            cart.direction = 'v';
+        } else if (trackCoordinate == '\\' && cart.direction == 'v') {
+            cart.direction = '>';
+        }
+
+        if (trackCoordinate == '+' && cart.direction == '<') {
+            if (cart.turn == Turn.left) {
+                cart.direction = 'v';
+            } else if (cart.turn == Turn.right) {
+                cart.direction = '^';
+            }
+            cart.nextTurn();
+        } else if (trackCoordinate == '+' && cart.direction == '^') {
+            if (cart.turn == Turn.left) {
+                cart.direction = '<';
+            } else if (cart.turn == Turn.right) {
+                cart.direction = '>';
+            }
+            cart.nextTurn();
+        } else if (trackCoordinate == '+' && cart.direction == '>') {
+            if (cart.turn == Turn.left) {
+                cart.direction = '^';
+            } else if (cart.turn == Turn.right) {
+                cart.direction = 'v';
+            }
+            cart.nextTurn();
+        } else if (trackCoordinate == '+' && cart.direction == 'v') {
+            if (cart.turn == Turn.left) {
+                cart.direction = '>';
+            } else if (cart.turn == Turn.right) {
+                cart.direction = '<';
+            }
+            cart.nextTurn();
+        }
+        this.coordinates[cart.yPosition][cart.xPosition] = cart.direction;
+        if (trackCoordinate != '/' && trackCoordinate != '\\' && trackCoordinate != '+' && trackCoordinate != '-' && trackCoordinate != '|') {
+            this.coordinates[cart.yPosition][cart.xPosition] = 'X';
+            this.firstCrash = cart.xPosition + ', ' + cart.yPosition;
+        }
+        for (let y = 0; y < this.coordinates.length; y++) {
+            let line : string = '';
+            for (let x = 0; x < this.coordinates[y].length; x++) {
+                line = line.concat(this.coordinates[y][x]);
+            }
+            console.log(y + ': ' + line);
+        };
+    }
+
+    constructor(input : string) {
+        let inputLines : string[] = input.split("\n");
+        let longestLine : number = 0;
+        for (let line of inputLines) {
+            if (line.length > longestLine) {
+                longestLine = line.length;
+            }
+        }
+        let y : number = 0;
+        for (let line of inputLines) {
+            this.coordinates[y] = [];
+            for (let x = 0; x < longestLine; x++) {
+                if (x >= line.length) {
+                    this.coordinates[y][x] = ' ';
+                } else {
+                    let currentChar : string = line.charAt(x);
+                    this.coordinates[y][x] = currentChar;
+                    if (currentChar == '^' || currentChar == 'v' || currentChar == '<' || currentChar == '>') {
+                        this.carts.add(new Cart(currentChar, x, y));
+                    }
+                }
+            }
+            y++;
+        }
+        let coordinatesLength : number = this.coordinates.length;
+            this.cleanTrackCoordinates  = new Array(coordinatesLength);
+        for (let i = 0; i < coordinatesLength; ++i) {
+            this.cleanTrackCoordinates[i] = [...this.coordinates[i]];
+        }
+        for (let y = 0; y < this.coordinates.length; y++) {
+            for (let x = 0; x < this.coordinates[y].length; x++) {
+                if (this.coordinates[y][x] == '^' || this.coordinates[y][x] == 'v') {
+                    this.cleanTrackCoordinates[y][x] = '|';
+                } else if (this.coordinates[y][x] == '<' || this.coordinates[y][x] == '>') {
+                    this.cleanTrackCoordinates[y][x] = '-';
+                }
+            }
+        }
+    }
+}
+
+export class Cart{
+
+    turn : Turn = Turn.left;
+
+    public nextTurn() {
+        if (this.turn == Turn.left) {
+            this.turn = Turn.straight;
+        } else if (this.turn == Turn.straight) {
+            this.turn = Turn.right;
+        } else if (this.turn == Turn.right) {
+            this.turn = Turn.left;
+        }
+    }
+
+    constructor(public direction : string, public xPosition : number, public yPosition : number) {
+    }
+}
+
+export enum Turn {
+    left, right, straight
+}
